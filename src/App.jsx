593eab6e4b66802309050acd48dd7c517a159cba
ucheson235 <emmyboi2010@yahoo.com>
2025-01-17 23:48:12 +0100

    create hearder componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router"
-import Home from "./Pages/Home"
-import Profile from "./Pages/Profile"
-import Offers from "./Pages/Offers"
-import SignIn from "./Pages/SignIn"
-import SignUp from "./Pages/SignUp"
-import ForgotPassword from "./Pages/ForgotPassword"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import Offers from "./Pages/Offers";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import ForgotPassword from "./Pages/ForgotPassword";
+import Header from "./components/Header";
 
 function App() {
-
-
   return (
-   
-      <div>
-       <Router>
-          <Routes>
-                 <Route path="/home" element={<Home/>}/>
-                 <Route path="/profile" element={<Profile/>}/>
-                 <Route path="/sign-in" element={<SignIn/>}/>
-                 <Route path="/sign-up" element={<SignUp/>}/>
-                 <Route path="/forgot-password" element={<ForgotPassword/>}/>
-                 <Route path="/offers" element={<Offers/>}/>
-
-                   
-          </Routes>
-       </Router>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/offers" element={<Offers />} />
        
-      </div>
-     
-   
-  )
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
