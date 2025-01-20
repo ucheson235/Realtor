bea57bba97fa88ce29e54dd74e453e5e39124c49
ucheson235 <emmyboi2010@yahoo.com>
2025-01-20 11:32:04 +0100

    updated route

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 export default function Header() {
-  const location = useLocation();
-  const navigate = useNavigate();
-
-  function pathMathRoute(route) {
-    if (route === location.pathname) {
+  const location = useLocation()
+  const navigate = useNavigate()
+  function pathMathRoute(route){
+    if(route === location.pathname){
       return true;
     }
   }
-
+ 
   return (
     <div className='bg-white border-b shadow-sm sticky top-0 z-50'>
-      <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
-        <div>
-          <h1 className='text-3xl text-red-600 cursor-pointer' onClick={()=>(navigate("/"))}>Realtor</h1>
-        </div>
-        <div>
+        <header className='flex justify-between items-center px-3 max-w-6xl mx-auto'>
+         <di>
+           <img src='https://files.oaiusercontent.com/file-JNsEPWFiLXi8S2QpuNqt2D?se=2025-01-20T09%3A58%3A52Z&sp=r&sv=2024-08-04&sr=b&rscc=max-age%3D604800%2C%20immutable%2C%20private&rscd=attachment%3B%20filename%3D13e156f5-dd48-4362-8b42-fe69a22ecd81.webp&sig=EaOCQZ3ncpfWXD2xtIGWUpXsFYsMwmOKA5zCP2ygSzk%3D' alt='logo' className='h-20 cursor-pointer rounded-full' onClick={()=> navigate("/")} />
+         </di>
+         <div>
           <ul className='flex space-x-10'>
-            <li onClick={()=> (navigate("/"))} className={`py-3 text-sm font-semibold 
-              text-gray-400 border-b-[3px] border-b-transparent cursor-pointer ${pathMathRoute("/") && "text-black border-b-red-500"}`}>
-              Home
-            </li>
-            <li onClick={()=> (navigate("/offers"))} className={`py-3 text-sm font-semibold 
-              text-gray-400 border-b-[3px] border-b-transparent cursor-pointer ${pathMathRoute("/offers") && "text-black border-b-red-500"}`}>
-              Offers
-            </li>
-            <li onClick={()=> (navigate("/sign-in"))} className={`py-3 text-sm font-semibold 
-              text-gray-400 border-b-[3px] border-b-transparent cursor-pointer ${pathMathRoute("/sign-in") && "text-black border-b-red-500"}`}>
-              Sign In
-            </li>
+             <li className={` cursor-pointer py-3 text-sm font-semibold text-gray-400
+              border-b-[3px] border-transparent${pathMathRoute("/")
+              && "text-black border-b-red-500"} `} onClick={()=> navigate("/")}>Home</li>
+             <li  className={` cursor-pointer py-3 text-sm font-semibold text-gray-400
+              border-b-[3px] border-transparent${pathMathRoute("/offers")
+              && "text-black border-b-red-500"} `} onClick={()=> navigate("/offers")} >Offers</li>
+             <li  className={`cursor-pointer py-3 text-sm font-semibold text-gray-400
+              border-b-[3px] border-transparent${pathMathRoute("/sign-in")
+              && "text-black border-b-red-500"} `} onClick={()=>navigate("sign-in")}>Sign in</li>
           </ul>
-        </div>
-      </header>
+         </div>
+
+        </header>
     </div>
-  );
+  )
 }
+
+
+
