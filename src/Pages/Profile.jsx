7e518094e6de9d5a8899885a809f088ff7b81ad5
ucheson235 <emmyboi2010@yahoo.com>
2025-02-07 15:29:26 +0100

    create the lisiting componetn

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -162,8 +162,9 @@ export default function Profile() {
 					{!loading && Array.isArray(listings) && listings.length > 0 ? (
 						// Render the listings if not empty
 						<div>
-						<h2 className="text-2xl text-center font-semibold">My Listings</h2>
-						<ul>
+						<h2 className="text-2xl text-center font-semibold mb-6 mt-6">My Listings</h2>
+						<ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3
+						xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6">
 							{listings.map((listing) => (
 							<ListingItem
 								key={listing.id}
--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,7 +1,57 @@
+import { Link } from "react-router-dom";
+import { FaMapMarkerAlt } from "react-icons/fa";
 
-
-export default function ListingItem({listing, id}) {
-  return (
-    <div>{listing.name}</div>
-  )
+export default function ListingItem({ listing, id }) {
+	return (
+		<li
+			className="br-white flex flex-col justify-between items-center
+    shadow-md hover:shadow-xl rounded-md overflow-hidden
+    transition-shadow duration-150 m-[10px]"
+		>
+			<Link className="contents" to={`/category/${listing.type}/${id}`}>
+				<img
+					className="h-[170px] w-full object-cover
+        hover:scale-105 transition-scale duration-200
+        ease-in "
+					loading="lazy"
+					src={listing.imageUrls[0]}
+					alt=""
+				/>
+				<div className="w-full p[10px]">
+					<div className="flex items-center space-x-1">
+						<FaMapMarkerAlt className="h-4 w-4 text-green-600" />
+						<p className="font-semibold text-sm mb-[2px] text-gray-600 truncate">
+							{listing.address}
+						</p>
+					</div>
+					<p className="font-semibold mt-0 text-xl truncate">{listing.name}</p>
+					<p className="text-[#457b9d] mt-2 font-semibold">
+						$
+						{listing.offer
+							? listing.discountedPrice
+									.toString()
+									.replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+							: listing.regularPrice
+									.tostring()
+									.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+						{listing.type === "rent" && " / month"}
+					</p>
+					<div className="flex items-center mt-[10px] space-x-3 mb-6">
+						<div className="flex items-center space-x-1">
+							<p className="font-semibold text-xs">
+								{listing.bedrooms > 1 ? `${listing.bedrooms} Beds` : "1 Bed"}
+							</p>
+						</div>
+						<div className="flex items-center space-x-1">
+							<p className="font-semibold text-xs ">
+								{listing.bathrooms > 1
+									? `${listing.bathrooms} Baths`
+									: "1 Bath"}
+							</p>
+						</div>
+					</div>
+				</div>
+			</Link>
+		</li>
+	);
 }
