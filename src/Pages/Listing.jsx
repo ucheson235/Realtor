95a7defd559c418162b7d8baf5378dabb07e4882
ucheson235 <emmyboi2010@yahoo.com>
2025-03-07 12:21:08 +0100

    add share icon and functions

--- src/Pages/Listing.jsx
+++ src/Pages/Listing.jsx
@@ -7,11 +7,13 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, EffectFade, Autoplay } from "swiper/modules";
 import "swiper/css/bundle";
 import "swiper/css/navigation"; 
+import { FaShare } from "react-icons/fa";
 
 export default function Listing() {
 	const params = useParams();
 	const [listing, setListing] = useState(null);
 	const [loading, setLoading] = useState(true);
+	const [shareLinkCopied, setShareLinkCopied] = useState(false);
 
 	useEffect(() => {
 		async function fetchListing() {
@@ -53,6 +55,22 @@ export default function Listing() {
                 <SwiperSlide></SwiperSlide>
                 <SwiperSlide></SwiperSlide>
 			</Swiper>
+			<div  className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 
+			rounded-full w-12 h-12 flex justify-center items-center">
+			  <FaShare className="text-lg text-slate-500" onClick={()=> {
+				  navigator.clipboard.writeText(window.location.href);
+				  setShareLinkCopied(true);
+				  setTimeout(() => {
+					  setShareLinkCopied(false);
+				  }, 2000);
+				  
+			  }} />
+			  {shareLinkCopied &&  <p className="fixed top-[23%] right-[5%]
+				font-semibold border-2 border-gray-400 bg-white
+				p-2 rounded-md z-10">Link Copied</p>}
+
+			     
+			</div>
 		</main>
 	);
 }
