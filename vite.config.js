e7dff727724d8dacda3b2d73bc9bf8db68ba1326
ucheson235 <emmyboi2010@yahoo.com>
2025-03-17 11:38:11 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Realtor/',
-  build: {
-    outDir: 'dist',
-  },
+  base: "/Realtor/",
+  build: { outDir: "dist" },
   server: {
     open: true,
-  }
-})
+    proxy: {
+      "/api": "http://localhost:3000",  
+    },
+  },
+});
 
 
